c635806a369dbaf99399215f5c531febe8c85744
Alex Allen <325@holbertonschool.com>
2018-04-12 19:29:55 +0000

    prevents execution upon importation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import add_0
-add = add_0.add
-
-a = 1
-b = 2
-
-print('{} + {} = {}'.format(a, b, add(a, b)))
+if __name__ == "__main__":
+    import add_0
+    add = add_0.add
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
