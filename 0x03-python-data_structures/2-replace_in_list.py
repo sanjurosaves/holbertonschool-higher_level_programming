b3431e647a6b7abc3870bacf4891f98a6507e5af
Alex Allen <325@holbertonschool.com>
2018-04-18 05:06:46 +0000

    fixes

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,5 +5,5 @@ def replace_in_list(my_list, idx, element):
     elif idx >= len(my_list):
         return (my_list)
     else:
-        my_list[idx] = 9
+        my_list[idx] = element
     return (my_list)
