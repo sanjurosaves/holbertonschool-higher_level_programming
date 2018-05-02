7b38b2bb01299b2d93bbea20a18dfec61862bd74
Alex Allen <325@holbertonschool.com>
2018-05-02 02:22:23 +0000

    add task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""defines Square class by private instance attribute 'size'"""
+
+
+class Square():
+    """square class"""
+
+    def __init__(self, size):
+        """instantiation"""
+        self.__size = size
