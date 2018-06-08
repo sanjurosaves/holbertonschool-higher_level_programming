2e160697fab9bb55918dff9379e3c14797c4fe7f
Alex Allen <325@holbertonschool.com>
2018-06-08 22:20:04 +0000

    pep8 fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,4 +82,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """override __str__ method to return customized string"""
-        return "[Rectangle] ({}) <{}>/<{}> - <{}>/<{}>".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[Rectangle] ({}) <{}>/<{}> - <{}>/<{}>".format
+    (self.id, self.__x, self.__y, self.__width, self.__height)
