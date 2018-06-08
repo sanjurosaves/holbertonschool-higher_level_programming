5e775603564dfd825e6640a93291d039c1829544
Alex Allen <325@holbertonschool.com>
2018-06-08 19:26:43 +0000

    initialcommit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""creates class Base"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ instantiation """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
