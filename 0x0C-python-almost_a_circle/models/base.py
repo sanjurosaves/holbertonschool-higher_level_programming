c53dd5ec1e1cdec51291b97a9682e1dd96eb031c
Alex Allen <325@holbertonschool.com>
2018-06-08 20:26:36 +0000

    added class documentation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,12 +4,13 @@
 
 
 class Base:
+    """ creating Base class """
     __nb_objects = 0
 
     def __init__(self, id=None):
         """ instantiation """
-        if id is not None:
-            self.id = id
-        else:
+        if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+        else:
+            self.id = id
