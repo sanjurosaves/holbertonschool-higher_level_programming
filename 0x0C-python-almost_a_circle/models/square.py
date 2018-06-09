52b22ddc5ec0f39f33dea1be0cb77aec3c57190e
Alex Allen <325@holbertonschool.com>
2018-06-09 23:58:07 +0000

    removing square attribute creation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,20 +11,20 @@ class Square(Rectangle):
         """ instantiate square """
         self.size = size
         super().__init__(size, size, x, y, id)
-
-    @property
-    def size(self):
-        """ getter for size """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """ setter for size """
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if (value < 0):
-            raise ValueError("y must be >= 0")
-        self.__size = value
+#
+#    @property
+#    def size(self):
+#       """ getter for size """
+#      return self.__size
+#
+#   @size.setter
+#  def size(self, value):
+#     """ setter for size """
+#    if not isinstance(value, int):
+#       raise TypeError("y must be an integer")
+#  if (value < 0):
+#     raise ValueError("y must be >= 0")
+# self.__size = value
 
     def __str__(self):
         """override __str__ method to return customized string"""
