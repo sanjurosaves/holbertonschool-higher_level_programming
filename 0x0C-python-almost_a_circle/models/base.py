7722b326b6411cac8c1bf36b9ae5a0242795076a
Alex Allen <325@holbertonschool.com>
2018-06-10 01:18:46 +0000

    add task 17 JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,11 @@ class Base:
                 list_for_file.append(obj.to_dictionary())
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(list_for_file))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns the list of the JSON string representation json_string """
+        if json_string is None or len(json_string) == 0:
+            return ("[]")
+        else:
+            return json.loads(json_string)
