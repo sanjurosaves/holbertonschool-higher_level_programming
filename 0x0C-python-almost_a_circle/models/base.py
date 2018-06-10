9ef6b951c0a448153fabfa527a2b8d4ee114c80e
Alex Allen <325@holbertonschool.com>
2018-06-10 01:12:53 +0000

    add task 16 json string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,14 @@ class Base:
             return ("[]")
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes json string rep of list_objs to file """
+        filename = cls.__name__ + ".json"
+        list_for_file = []
+        if list_objs is not None:
+            for obj in list_objs:
+                list_for_file.append(obj.to_dictionary())
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(list_for_file))
