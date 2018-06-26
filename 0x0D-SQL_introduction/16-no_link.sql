f067645c6ecf4b59ceded3d779b29bf522fdc3fa
Alex Allen <325@holbertonschool.com>
2018-06-26 06:23:50 +0000

    correct case

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- lists all records of the table second_table by score, top first, omits NULL names
 -- use SELECT ... FROM ... ORDER BY
-SELECT score, name FROM second_table where name is not NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
