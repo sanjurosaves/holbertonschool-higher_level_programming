d4f05904ec2df097589d143f3f8ef3311c2255f5
Alex Allen <325@holbertonschool.com>
2018-07-20 21:05:46 +0000

    trying it with a list

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,17 +6,16 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    lst = []
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.name from cities \
-    INNER JOIN states on cities.state_id = states.id \
+    INNER JOIN states ON cities.state_id = states.id \
     WHERE states.name LIKE BINARY %s ORDER BY cities.id", (argv[4],))
     cities = cur.fetchall()
     citycount = len(cities)
-    for i in range(citycount):
-        if i < (citycount - 1):
-            print(cities[i][0], end=', ')
-        else:
-            print(cities[i][0])
+    for city in cities:
+        lst.append(city[0])
+    print(', '.join(lst))
     cur.close()
     db.close()
