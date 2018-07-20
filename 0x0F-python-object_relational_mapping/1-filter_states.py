3840f6e47e0fd0d5c7c6725a5b57fedde38a0b6d
Alex Allen <325@holbertonschool.com>
2018-07-20 20:19:39 +0000

    trying LIKE BINARY

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
                          host="localhost", port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states\
+    WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     states = cur.fetchall()
     for state in states:
         print(state)
