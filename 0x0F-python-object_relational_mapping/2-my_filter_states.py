c7b3935ebf0d86459492db238de804bca0ddae53
Alex Allen <325@holbertonschool.com>
2018-07-20 20:32:04 +0000

    like binary is better than = because it more accurately handles trailing spaces

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{:s}' \
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{:s}' \
                 ORDER BY id ASC".format(argv[4]))
     states = cur.fetchall()
     for state in states:
