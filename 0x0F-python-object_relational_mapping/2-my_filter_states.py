faeea95bbdb94dd2d2a2abac0d47754fa3111062
Alex Allen <325@holbertonschool.com>
2018-07-20 20:29:59 +0000

    add sort

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,8 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{:s}'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name='{:s}' \
+                ORDER BY id ASC".format(argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
