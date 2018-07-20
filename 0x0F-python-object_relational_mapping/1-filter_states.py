4c8b540f7f106992bff0988a2a0802b2277bc56d
Alex Allen <325@holbertonschool.com>
2018-07-20 20:16:07 +0000

    add port and localhost name

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3],
+                         host="localhost", port=3306)
     cur = db.cursor()
     cur.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY id ASC")
     states = cur.fetchall()
