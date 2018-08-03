16c0ff2b2dd2fe6259f9816def5f6ce029e43c1e
Alex Allen <325@holbertonschool.com>
2018-08-03 17:43:37 +0000

    fix logical issues

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,22 +3,21 @@
 
 def find_peak(list_of_integers):
     l = list_of_integers
-    m = int(len(l) / 2)
-
-    # print(m)
+    m = len(l) // 2
+    print("mid is {}".format(m))
 
     if not l:
         return None
-    elif (l[m] >= l[m-1]) or (l[m] >= l[m+1]):
+    elif (l[m] >= l[m-1]) and (l[m] >= l[m+1]):
         return l[m]
-    elif l[m] < l[m-1]:
+    elif (m > 0) and (l[m] <= l[m-1]):
         # iterate through left half of list
         i = m - 1
         while i > 0:
             if l[i] >= l[i - 1]:
                 return l[i]
             i = i - 1
-    elif l[m] < l[m+1]:
+    else: #if l[m] < l[m+1]:
         # iterate through right half of list
         i = m + 1
         while i < len(l):
