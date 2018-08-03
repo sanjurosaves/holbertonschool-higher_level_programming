73863befe77a0293b90691714ee08036ff2c8f2e
Alex Allen <325@holbertonschool.com>
2018-08-03 18:17:38 +0000

    i've seen that movie too

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,6 +10,10 @@ def find_peak(list_of_integers):
         return None
     elif (l[m] >= l[m-1]) and (l[m] >= l[m+1]):
         return l[m]
+    elif l[0] >= l[1]:
+        return l[0]
+    elif l[-1] >= l[-2]:
+        return l[-1]
     elif (m > 0) and (l[m] <= l[m-1]):
         # iterate through left half of list
         i = m - 1
@@ -20,7 +24,7 @@ def find_peak(list_of_integers):
     else: #if l[m] < l[m+1]:
         # iterate through right half of list
         i = m + 1
-        while i < len(l):
+        while i < (len(l) - 1):
             if l[i] >= l[i + 1]:
                 return l[i]
             i = i + 1
