156cbc60b5d4472a4cd3a62cda2d2556b28a4cc8
Alex Allen <325@holbertonschool.com>
2018-08-06 20:57:12 +0000

    simple if else

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,11 +6,8 @@ if __name__ == '__main__':
 
     url = sys.argv[1]
 
-    try:
-        r = requests.get(url)
-        if r.status_code == 404:
-            print("Error code: 404")
-        else:
-            print(r.text)
-    except HTTPError:
-        print("Error code: {}".format(r.raise_for_status()))
+    r = requests.get(url)
+    if r.status_code != 200:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
