425338ec7cde00d704e6cef61d1390b5b9ecbca9
Alex Allen <325@holbertonschool.com>
2018-08-06 18:57:17 +0000

    include main wrapper

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,11 +6,11 @@ sends a request to the URL and displays the value of the
 X-Request-Id variable found in the header of the response
 """
 
-
-url = sys.argv[1]
-data = None
-headers = {'User-Agent': 'Python-urllib/3.4'}
-req = urllib.request.Request(url, data, headers)
-with urllib.request.urlopen(req) as response:
-    page = response.info()
-print(page['X-Request-Id'])
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = None
+    headers = {'User-Agent': 'Python-urllib/3.4'}
+    req = urllib.request.Request(url, data, headers)
+    with urllib.request.urlopen(req) as response:
+        page = response.info()
+    print(page['X-Request-Id'])
