020ec15f48e06b2acfba9f1c0bc3bc1ef66c76d2
Alex Allen <325@holbertonschool.com>
2018-08-06 21:57:15 +0000

    add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    from requests.auth import HTTPBasicAuth
+
+    u = sys.argv[1]
+    p = sys.argv[2]
+    url = 'https://api.github.com/users/'+u
+
+    r = requests.get(url, auth=HTTPBasicAuth(u, p))
+
+    jso = r.json()
+    print(jso['id'])
