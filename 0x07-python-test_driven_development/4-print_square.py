f9e6d05993133a5097f8a6947e3e8f09e17cc012
Alex Allen <325@holbertonschool.com>
2018-08-25 01:09:07 +0000

    add tas 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' module for print_square method '''
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for l in range(size):
+        for q in range(size):
+            print('#', end='')
+        print()
