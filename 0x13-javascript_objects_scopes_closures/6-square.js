f41055dd5db0475e396a942210efd2ea4c34e123
Alex Allen <325@holbertonschool.com>
2018-09-06 18:38:38 +0000

    extending baseSquare instead of using prototype

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
-const Square = require('./5-square');
+const OrigSquare = require('./5-square');
 
-Square.prototype.charPrint = function (c) {
-  if (c === undefined) {
-    c = 'X';
-  }
+class Square extends OrigSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
 
-  let i;
-  for (i = 0; i < this.height; i++) {
-    console.log(c.repeat(this.width));
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
-};
+}
 
 module.exports = Square;
